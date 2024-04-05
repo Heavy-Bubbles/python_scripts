e8ddd5c4066f9cab37da7bc6082c2f6c7a4423f2
Heavy-Bubbles <chelseatanxuanlin@gmail.com>
2024-04-06 00:35:39 +0800

    added file manipulation

--- Extracting List From Webpage/extract_list.py
+++ Extracting List From Webpage/extract_list.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import shutil, os
 
 scraper = pd.read_html('https://en.wikipedia.org/wiki/List_of_sovereign_states')
 
@@ -9,4 +10,18 @@ scraper = pd.read_html('https://en.wikipedia.org/wiki/List_of_sovereign_states')
 #     print(idx)
 #     print(table)
 
-scraper[1].to_csv("ListOfSovereignStates.csv")
+list_of_sovereign_states = scraper[1]
+
+# panda dataframe
+df = pd.DataFrame(list_of_sovereign_states)
+print(df)
+
+file_name = "ListOfSovereignStates.csv"
+
+directory = os.getcwd()
+file_path = os.path.join(directory, file_name)
+
+if os.path.exists(file_path):
+    os.remove(file_path)
+
+new_file = df.to_csv(file_name)
